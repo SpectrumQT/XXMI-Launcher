8c72f6c38bb29193e5feff26e7940854bbd6dd0b
SpectrumQT <19625504+SpectrumQT@users.noreply.github.com>
2024-10-06 18:29:15 +0700

    Switched launcher auto update target to msi installer

--- src/xxmi_launcher/core/packages/launcher_package.py
+++ src/xxmi_launcher/core/packages/launcher_package.py
@@ -1,5 +1,8 @@
+import shutil
 import sys
 import logging
+import subprocess
+import time
 
 from dataclasses import dataclass
 
@@ -8,6 +11,8 @@ import core.event_manager as Events
 import core.config_manager as Config
 
 from core.package_manager import Package, PackageMetadata
+
+from core.utils.process_tracker import wait_for_process, WaitResult
 
 log = logging.getLogger(__name__)
 
@@ -39,7 +44,7 @@ class LauncherPackage(Package):
             github_repo_owner='SpectrumQT',
             github_repo_name='XXMI-Launcher',
             asset_version_pattern=r'.*(\d\.\d\.\d).*',
-            asset_name_format='XXMI-LAUNCHER-PACKAGE-v%s.zip',
+            asset_name_format='XXMI-Launcher-Installer-Online-v%s.msi',
             signature_pattern=r'^## Signature[\r\n]+- ((?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{4}|[A-Za-z0-9+\/]{3}=|[A-Za-z0-9+\/]{2}={2})$)',
             signature_public_key='MHYwEAYHKoZIzj0CAQYFK4EEACIDYgAEYac352uRGKZh6LOwK0fVDW/TpyECEfnRtUp+bP2PJPP63SWOkJ3a/d9pAnPfYezRVJ1hWjZtpRTT8HEAN/b4mWpJvqO43SAEV/1Q6vz9Rk/VvRV3jZ6B/tmqVnIeHKEb',
             exit_after_update=True,
@@ -47,6 +52,7 @@ class LauncherPackage(Package):
         installed_version = self.get_installed_version()
         if Config.Launcher.config_version < installed_version:
             Config.Config.upgrade(installed_version)
+            self.cleanup_old_version()
 
     def get_installed_version(self):
         if getattr(sys, 'frozen', False):
@@ -54,10 +60,45 @@ class LauncherPackage(Package):
         else:
             return '0.0.0'
 
-    def download_latest_version(self):
-        # This package is supposed to be downloaded via Installer
-        pass
-
     def install_latest_version(self, clean):
         Events.Fire(Events.PackageManager.InitializeInstallation())
-        Events.Fire(Events.InstallerManager.UpdateLauncher())
+
+        subprocess.Popen(f'msiexec /i "{self.downloaded_asset_path}" /qr APPDIR="{Paths.App.Root}" CREATE_SHORTCUTS=""', shell=True)
+
+        installer_process_name = 'EnhancedUI.exe'
+
+        Events.Fire(Events.Application.StatusUpdate(status='Waiting for installer to start...'))
+
+        result, pid = wait_for_process(installer_process_name, with_window=True, timeout=15)
+        if result == WaitResult.Timeout:
+            raise ValueError(f'Failed to start {self.downloaded_asset_path.name}!\n\n'
+                             f'Was it blocked by Antivirus software or security settings?')
+
+        time.sleep(1)
+
+    def cleanup_old_version(self):
+        # Cleanup pre-0.9.7
+        old_exe_path = Paths.App.Root / 'XXMI Launcher.exe'
+        if old_exe_path.is_file():
+            Events.Fire(Events.Application.StatusUpdate(status='Removing old files...'))
+            # Remove pre-0.9.7 files and folders from `XXMI Launcher/Resources`
+            for path in Paths.App.Resources.iterdir():
+                if path.name in ['Bin', 'Packages', 'Security']:
+                    continue
+                if path.is_dir():
+                    shutil.rmtree(path)
+                else:
+                    path.unlink()
+                time.sleep(0.01)
+            # Remove pre-0.9.7 Installer
+            installer_path = Paths.App.Resources / 'Packages' / 'Installer'
+            if installer_path.is_dir():
+                shutil.rmtree(installer_path)
+            # Remove pre-0.9.7 exe
+            old_exe_path.unlink()
+            # Notify user about new exe path
+            msg = ''
+            msg += f'Launcher .exe file location was changed to:\n\n'
+            msg += f'{Paths.App.Resources / "Bin" / "XXMI Launcher.exe"}\n\n'
+            msg += f'Desktop shortcut was updated automatically. Sorry for bothering!'
+            Events.Fire(Events.Application.ShowInfo(title='Update Notification', message=msg))
