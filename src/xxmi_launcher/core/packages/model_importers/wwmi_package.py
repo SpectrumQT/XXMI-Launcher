e4fdb750f417a197a0b46435362519776df81afe
SpectrumQT <19625504+SpectrumQT@users.noreply.github.com>
2025-03-30 05:17:48 +0700

    Added extra tweaks to WWMI FPS unlocker

--- src/xxmi_launcher/core/packages/model_importers/wwmi_package.py
+++ src/xxmi_launcher/core/packages/model_importers/wwmi_package.py
@@ -423,7 +423,7 @@ class SettingsManager:
             if 'CustomFrameRate' in trigger.body:
                 self.db.delete_trigger(trigger.name)
 
-        # Ensure that our trigger is keeping correct fps value
+        # Ensure that our trigger is keeping correct FPS value
         if lock_exist:
             fps = self.db.get_value('CustomFrameRate')
             if fps != str(value):
@@ -432,12 +432,46 @@ class SettingsManager:
             else:
                 return
 
-        # Remove unneeded old entries from the table that are making game to ignore our fps setting
-        self.db.delete_value('MenuData')
-        self.db.delete_value('PlayMenuInfo')
-        self.db.delete_value('IsConvertOldMenuData')
+        # Mix in the magic data (kudos to WakuWakuPadoru for dumping it), else some clients just refuse to go 120 FPS
+        self.db.set_value('MenuData', json.dumps({
+            '___MetaType___': '___Map___',
+            'Content': [
+                [1, 100], [2, 100], [3, 100], [4, 100], [5, 0], [6, 0],
+                [7, -0.4658685302734375], [10, 3], [11, 3], [20, 0], [21, 0],
+                [22, 0], [23, 0], [24, 0], [25, 0], [26, 0], [27, 0], [28, 0],
+                [29, 0], [30, 0], [31, 0], [32, 0], [33, 0], [34, 0], [35, 0],
+                [36, 0], [37, 0], [38, 0], [39, 0], [40, 0], [41, 0], [42, 0],
+                [43, 0], [44, 0], [45, 0], [46, 0], [47, 0], [48, 0], [49, 0],
+                [50, 0], [51, 1], [52, 1], [53, 0], [54, 3], [55, 1], [56, 2],
+                [57, 1], [58, 1], [59, 1], [61, 0], [62, 0], [63, 1], [64, 1],
+                [65, 0], [66, 0], [67, 3], [68, 2], [69, 100], [70, 100], [79, 1],
+                [81, 0], [82, 1], [83, 1], [84, 0], [85, 0], [87, 0], [88, 0],
+                [89, 50], [90, 50], [91, 50], [92, 50], [93, 1], [99, 0], [100, 30],
+                [101, 0], [102, 1], [103, 0], [104, 50], [105, 0], [106, 0.3],
+                [107, 0], [112, 0], [113, 0], [114, 0], [115, 0], [116, 0],
+                [117, 0], [118, 0], [119, 0], [120, 0], [121, 1], [122, 1],
+                [123, 0], [130, 0], [131, 0], [132, 1], [135, 1], [133, 0]
+            ]
+        }))
+        self.db.set_value('PlayMenuInfo', json.dumps({
+            '1': 100, '2': 100, '3': 100, '4': 100, '5': 0, '6': 0,
+            '7': -0.4658685302734375, '10': 3, '11': 3, '20': 0, '21': 0,
+            '22': 0, '23': 0, '24': 0, '25': 0, '26': 0, '27': 0, '28': 0,
+            '29': 0, '30': 0, '31': 0, '32': 0, '33': 0, '34': 0, '35': 0,
+            '36': 0, '37': 0, '38': 0, '39': 0, '40': 0, '41': 0, '42': 0,
+            '43': 0, '44': 0, '45': 0, '46': 0, '47': 0, '48': 0, '49': 0,
+            '50': 0, '51': 1, '52': 1, '53': 0, '54': 3, '55': 1, '56': 2,
+            '57': 1, '58': 1, '59': 1, '61': 0, '62': 0, '63': 1, '64': 1,
+            '65': 0, '66': 0, '67': 3, '68': 2, '69': 100, '70': 100, '79': 1,
+            '81': 0, '82': 1, '83': 1, '84': 0, '85': 0, '87': 0, '88': 0,
+            '89': 50, '90': 50, '91': 50, '92': 50, '93': 1, '99': 0, '100': 30,
+            '101': 0, '102': 1, '103': 0, '104': 50, '105': 0, '106': 0.3,
+            '107': 0, '112': 0, '113': 0, '114': 0, '115': 0, '116': 0,
+            '117': 0, '118': 0, '119': 0, '120': 0, '121': 1, '122': 1,
+            '123': 0, '130': 0, '131': 0, '132': 1
+        }))
 
-        # Set fps setting and lock it from further changes via trigger
+        # Set FPS setting and lock it from further changes via trigger
         self.set_setting('CustomFrameRate', str(value), True)
 
     def reset_fps_setting(self):
