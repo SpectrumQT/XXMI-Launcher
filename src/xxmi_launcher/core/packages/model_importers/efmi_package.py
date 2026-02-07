582eae60bfc9ff7fcd7fa13b6790b240491a53ee
SpectrumQT <19625504+SpectrumQT@users.noreply.github.com>
2026-02-07 08:33:47 +0700

    Fixed wrong EFMI-Package public key

--- src/xxmi_launcher/core/packages/model_importers/efmi_package.py
+++ src/xxmi_launcher/core/packages/model_importers/efmi_package.py
@@ -95,7 +95,7 @@ class EFMIPackage(ModelImporterPackage):
             asset_version_pattern=r'.*(\d\.\d\.\d).*',
             asset_name_format='EFMI-PACKAGE-v%s.zip',
             signature_pattern=r'^## Signature[\r\n]+- ((?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{4}|[A-Za-z0-9+\/]{3}=|[A-Za-z0-9+\/]{2}={2})$)',
-            signature_public_key='MHYwEAYHKoZIzj0CAQYFK4EEACIDYgAEeigvK7REsX3f/vb+RRuFkZt/6VRbykI2oQcEU3IiI3N9s6jWqKkxAE2cTC9wKXDlkeSzlHjPxgzrTrKdqwkFzROMjw5T2LixFB5BYaT633aU/cCiHDbArIJ46+GrqemG',
+            signature_public_key='MHYwEAYHKoZIzj0CAQYFK4EEACIDYgAEYac352uRGKZh6LOwK0fVDW/TpyECEfnRtUp+bP2PJPP63SWOkJ3a/d9pAnPfYezRVJ1hWjZtpRTT8HEAN/b4mWpJvqO43SAEV/1Q6vz9Rk/VvRV3jZ6B/tmqVnIeHKEb',
             exit_after_update=False,
             installation_path='EFMI/',
             requirements=['XXMI'],
